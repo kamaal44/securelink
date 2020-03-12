ab8a8a439e7fec40156681fc02317119c3524666
Nik Krumm <nkrumm@gmail.com>
2020-03-12 10:33:12 -0700

    Some basic form validation and error handling

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # serve homepage
-    return render_template('index.html')
+    barcode = request.args.get('code', "")
+    return render_template('index.html', barcode=barcode)
 
 
 @app.route('/result', methods=['POST'])
 def show_result():
     barcode = request.form['barcode']
     dob = request.form['dob']
-    
-    
+
+
     return render_template('results.html', barcode=barcode, collect_dt="03/10/2020", result="POSITIVE")
