fee07819bec0da2027ccd3932736a701c21321f3
Nik Krumm <nkrumm@gmail.com>
2020-03-12 00:03:47 -0700

    add flask, templates, /results route,

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # serve homepage
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST'])
+def show_result():
+    barcode = request.form['barcode']
+    dob = request.form['dob']
+    
+    
+    return render_template('results.html', barcode=barcode, collect_dt="03/10/2020", result="POSITIVE")
